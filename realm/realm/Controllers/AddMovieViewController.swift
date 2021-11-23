277a4aa63420893489ffd4f92c8505b4aae51017
Ömer Sezer <omer.sezer@mobilistbilisim.com>
2021-11-23 12:28:30 +0300

    realm project added

---
+++ realm/realm/Controllers/AddMovieViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddMovieViewController.swift
+//  realm
+//
+//  Created by omersezer on 23.11.2021.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
