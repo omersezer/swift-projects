7a6853d6d4c62b823a4cec8eaa3a399c0495b977
Ömer Sezer <oomersezer@gmail.com>
2022-02-27 16:49:23 +0300

    project finished

--- singleton/singleton/AppDelegate.swift
+++ singleton/singleton/AppDelegate.swift
@@ -9,11 +9,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = SplashViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
