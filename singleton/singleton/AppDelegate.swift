726df27d4ab7ef1fe39cfd59849c2692a1dcd360
Ömer Sezer <oomersezer@gmail.com>
2022-02-27 16:22:26 +0300

    project created

---
+++ singleton/singleton/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  singleton
+//
+//  Created by Ömer Sezer on 27.02.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
