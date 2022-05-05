b01aec8efa53627e54426d72672e190844fe76c7
Ömer Sezer <oomersezer@gmail.com>
2022-05-05 18:06:44 +0300

    ui menu project has been added (#6)

---
+++ uimenu/uimenu/Extensions/UIImage+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extension.swift
+//  uimenu
+//
+//  Created by Ömer Sezer on 5.05.2022.
+//
+
+import UIKit.UIImage
+
+extension UIImage {
+    static let share = UIImage(systemName: "square.and.arrow.up")
+    static let more = UIImage(systemName: "ellipsis")
+    static let add = UIImage(systemName: "plus")
+    static let edit = UIImage(systemName: "pencil")
+    static let report = UIImage(systemName: "exclamationmark.bubble")
+    static let delete = UIImage(systemName: "trash")
+}
